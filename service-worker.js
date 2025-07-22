1f09a63067225baeb7a1c3c9a60bf40fd7983722
lugengar <luciano.barbinii@gmail.com>
2025-07-21 23:57:02 -0300

    a

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// service-worker.js
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+      caches.open('mi-cache').then(function (cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/icon-192.png',
+          '/icon-512.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.match(event.request).then(function (response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
