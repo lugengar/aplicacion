6446e918e2a98dd2cfa50ba2bf04050251f5fdc1
lugengar <luciano.barbinii@gmail.com>
2025-07-22 01:50:38 -0300

    a

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,29 @@
-// service-worker.js
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('mi-cache-v1').then(function(cache) {
+    caches.open('mi-cache-v1').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
         '/manifest.json',
         '/icon-512.png',
-        // Agrega aquí todos los archivos estáticos que uses
+        // otros archivos estáticos que uses
       ]);
     })
   );
 });
 
-  
-  self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  if (url.href === 'https://lugengar.github.io/aplicacion/data.json') {
+    // Para esta URL, solo fetch directo (sin cache)
+    event.respondWith(fetch(event.request));
+  } else {
+    // Para todo lo demás, cache primero
     event.respondWith(
-      caches.match(event.request).then(function (response) {
+      caches.match(event.request).then(response => {
         return response || fetch(event.request);
       })
     );
-  });
-  
+  }
+});
