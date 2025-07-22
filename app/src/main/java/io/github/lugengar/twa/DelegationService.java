e2f395fb2a84261ba6c995c39166d6a202948384
lugengar <luciano.barbinii@gmail.com>
2025-07-22 01:35:04 -0300

    a

---
+++ app/src/main/java/io/github/lugengar/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.lugengar.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
